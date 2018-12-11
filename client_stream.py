4b9764244e59cff8a0479fb78206ff517ba544e9
FrancescoDAlterio <francesco.dalterio1992@gmail.com>
2018-12-11 10:42:22 -0500

    embedding iperf (work in progress)

--- client_stream.py
+++ client_stream.py
@@ -19,12 +19,12 @@ CLIENT_IPERF_PORT = 5210 #actually it runs iperf server
 #global variables
 
 pipe_name = 'pipe_iperf'
-udp_stream_active = True
+keep_executing = True
 
 transfer = -1
 bandwidth=-1
 jitter = -1
-lost_perc= -1
+received_perc= -1
 
 '''
 def rcv_udp_thread(udp_sock):
@@ -45,37 +45,61 @@ def rcv_udp_thread(udp_sock):
 
 ##### END DECLARATION PART #####
 
+
 def UDPiperfthread(iperf_port):
 
     #in theory this thread should never terminate
     print "UDPiperfthread:  launching UDP test with iperf3 server"
 
-    global udp_stream_active
-
+    global keep_executing
     pipeout = os.open(pipe_name, os.O_WRONLY)
     process_to_open = 'stdbuf -oL iperf3 -s -V' #default port 5201
 
-    udp_stream_active = True #add some delay after checking this variable
+
     p = subprocess.Popen(process_to_open, shell=True, stdout=pipeout)
+
     p.wait()
     #udp_stream_active = False
-    #print "iperf3 SERVER HAS CRASHED"
-    while True:
+
+    while keep_executing:
         time.sleep(1)
+
+    print "UDPiperfthread:  Exiting..."
+
+
+
 
 def computemetricsthread():
 
+    # in theory this thread should never terminate
     print "computemetricsthread: generating metrics..."
     pipein = open(pipe_name, 'r')
+    global keep_executing
+
     global transfer
     global bandwidth
     global jitter
-    global lost_perc
+    global received_perc
 
-    while udp_stream_active:
+    while keep_executing:
+
 
         line = pipein.readline()[:-1]
+
+        if line == "iperf3: the client has terminated":
+            print "ERROR: server not reachable anymore"
+            keep_executing = False
+            print "keep_executing:",keep_executing
+            break
+
+
         tokenize = line.split()
+
+        if len(tokenize)>2 and  tokenize [1] == "error":
+            print line
+            keep_executing = False
+            print "keep_executing:", keep_executing
+            break
 
         #real metrics
         if len(tokenize)>3 and tokenize[3] == 'sec':
@@ -83,12 +107,12 @@ def computemetricsthread():
             transfer = ClientUtilities.toKilo(tokenize[5],tokenize[4])
             bandwidth = ClientUtilities.toKilo(tokenize[7],tokenize[6])
             jitter = ClientUtilities.str_to_float(tokenize[8])[1]
-            lost_perc = ClientUtilities.str_to_float(tokenize[11].translate(None, '()%'))[1]
+            received_perc = 100-ClientUtilities.str_to_float(tokenize[11].translate(None, '()%'))[1]
 
-            print "transfer:",transfer," bandwidth:",bandwidth," jitter:",jitter,"lost_perc:",lost_perc
+            print "transfer:", transfer," bandwidth:", bandwidth," jitter:", jitter,"received_perc:", received_perc
 
 
-    print "Finished computing metrics"
+    print "computemetricsthread: Finished computing metrics...Exiting"
 
 
 #create TCP control socket
@@ -137,6 +161,9 @@ except SocketError as e:
     print
     "Exception name:", type(e).__name__, " ", e.args
 
+    keep_executing = False
+    print "keep_executing:", keep_executing
+
     sys.exit()
 
 
@@ -149,7 +176,7 @@ while 1:
         rand_num=(random.randint(0, 10))
         print "generated number %d" %(rand_num)
 
-        cl_control_sock.send(str(rand_num).encode()) #FORSE DA ERRORE
+        cl_control_sock.send(str(rand_num).encode())
 
         time.sleep(1)
 
@@ -162,7 +189,16 @@ while 1:
         print "ERROR:Connection closed by the server, exit"
         print "Exception name:", type(e).__name__, " ", e.args
 
+        keep_executing = False
+        print "keep_executing:", keep_executing
+
         break
+
+    except KeyboardInterrupt:
+
+        print "EXECUTION STOPPED BY THE USER"
+        keep_executing = False
+        sys.exit()
 
     """
     except BrokenPipeError as e:
